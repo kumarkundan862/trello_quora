6cd085962a739fb31ea025fa28f80da2483f1b57
AditiKonde <aditi.konde.25@gmail.com>
2020-12-25 00:05:25 +0530

    pom file updated with dependencies

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package com.upgrad.quora.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable
+
+@Entity
+@Table(name = "USERS", schema = "quora")
+public class UserEntity implements Serializable {
+
+//    public UserEntity() {}
+//
+//    @Id
+//    @Column(name = "id")
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    @Column(name = "uuid")
+//    @Size(max = 64)
+//    private String uuid;
+
+
+
+}
