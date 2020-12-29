320bc1d08e73c05e135ac52a35646e62ef789e76
aishwarya-bm <biradaraishwarya@gmail.com>
2020-12-29 17:07:26 +0530

    generated getters and setters for Question & Answer entities

--- quora-service/src/main/java/com/upgrad/quora/service/entity/Answer.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/Answer.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "answer")
@@ -36,4 +37,34 @@ public class Answer {
     @JoinColumn(name = "question_id")
     @NotNull
     private Question question;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answer answer = (Answer) o;
+        return id == answer.id &&
+                Objects.equals(uuid, answer.uuid) &&
+                Objects.equals(user, answer.user) &&
+                Objects.equals(ans, answer.ans) &&
+                Objects.equals(date, answer.date) &&
+                Objects.equals(question, answer.question);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uuid, user, ans, date, question);
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", uuid='" + uuid + '\'' +
+                ", user=" + user +
+                ", ans='" + ans + '\'' +
+                ", date=" + date +
+                ", question=" + question +
+                '}';
+    }
 }
