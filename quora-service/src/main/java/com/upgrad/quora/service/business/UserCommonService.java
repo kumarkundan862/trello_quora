b9d06d40bb0699ea0c36a143f68f9f645ab29a01
Sehbaz Rafik <sehbazm@gmail.com>
2020-12-29 09:22:00 +0000

    Common endpoints with auth

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserCommonService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserCommonService.java
@@ -7,6 +7,10 @@ import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
 
 @Service
 public class UserCommonService {
@@ -14,34 +18,30 @@ public class UserCommonService {
     @Autowired
     private UserDao userDao;
 
-    @Autowired
-    private UserAuthTokenValidifierService userAuthTokenValidifierService;
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity getUser(final String userUuid, final String authorizationToken) throws AuthorizationFailedException,
+            UserNotFoundException {
+        UserAuthEntity userAuthEntity = userDao.getUserByAccessToken(authorizationToken);
+        System.out.println(userAuthEntity);
+        if (userAuthEntity != null) {
 
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime loggedOutTime = userAuthEntity.getLogoutAt();
+            final long difference = now.compareTo(loggedOutTime);
 
-    public UserEntity getUser(final String userUuid, final String authorizationToken) throws
-            AuthorizationFailedException, UserNotFoundException {
+            System.out.println(userAuthEntity);
 
-        UserAuthEntity userAuthTokenEntity = userDao.getUserAuthToken(authorizationToken);
-/*
-        //Check if user has signed-in
-        if (userAuthTokenEntity == null) {
-            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+            if (difference < 0) {
+                UserEntity userEntity = userDao.getUserByUuid(userUuid);
+                if (userEntity != null) {
+                    return userEntity;
+                }
+                throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
+            }
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out");
         }
-
-        //Check if user has signed-out
-        if (userAuthTokenValidifierService.userSignOutStatus(authorizationToken)) {
-            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
-        }
-
- */
-
-        //Check if user is present
-        UserEntity userEntity = userDao.getUserByUserName(userUuid);
-        if (userEntity == null) {
-            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
-        }
-
-        return userEntity;
+        throw new AuthorizationFailedException("USR-001", "User has not signed in");
     }
+
 
 }
