a7c94ddc63cf1cb481310931dcd8772279690de5
kumarkundan862 <kumarkundan862@gmail.com>
2020-12-30 19:05:34 +0530

    added files Questioncontroller,QuestionService,QuestionDao,Question to implement editQuestion

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -1,8 +1,10 @@
 package com.upgrad.quora.service.dao;
 
+import com.upgrad.quora.service.entity.Question;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -10,4 +12,17 @@ public class QuestionDao {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    public Question getQuestionByUuid(final String Uuid) {
+        try {
+            return entityManager.createNamedQuery("questionByUuid", Question.class).setParameter(
+                    "uuid", Uuid).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public void editQuestion(final Question question) {
+        entityManager.merge(question);
+    }
 }
